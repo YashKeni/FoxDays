86034b0eb5297da445c41e6579d6df2db60498d8
YashKeni <yashkeni007@gmail.com>
2023-09-14 14:53:17 +0530

    Installed Input System

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [Header("Player Settings")]
+    [SerializeField] float runSpeed = 5f;
+
+    Vector2 moveInput;
+    Rigidbody2D myRigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Run();
+        FlipSideWays();
+    }
 
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void Run()
+    {
+        Vector2 playerVelo = new Vector2(moveInput.x * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelo;
+    }
+
+    void FlipSideWays()
+    {
+        bool hasHorizSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+
+        if (hasHorizSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
     }
 }
