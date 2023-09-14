38fdc4aa92aba7dab32c23b2090e07b7276c7314
YashKeni <yashkeni007@gmail.com>
2023-09-14 23:19:26 +0530

    Added Jump function to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,20 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     [Header("Player Settings")]
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 2f;
 
     Vector2 moveInput;
     Rigidbody2D myRigidBody;
+    Animator animator;
+    CapsuleCollider2D bodyCollider;
+    BoxCollider2D footCollider;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        bodyCollider = GetComponent<CapsuleCollider2D>();
+        footCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -25,15 +33,14 @@ public class Player : MonoBehaviour
         FlipSideWays();
     }
 
-    void OnMove(InputValue value)
-    {
-        moveInput = value.Get<Vector2>();
-    }
 
     void Run()
     {
         Vector2 playerVelo = new Vector2(moveInput.x * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelo;
+
+        bool hasHorizSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        animator.SetBool("isRunning", hasHorizSpeed);
     }
 
     void FlipSideWays()
@@ -43,6 +50,22 @@ public class Player : MonoBehaviour
         if (hasHorizSpeed)
         {
             transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
+    }
+
+    // Player Input Methods
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (!footCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+
+        if (value.isPressed)
+        {
+            myRigidBody.velocity += new Vector2(0f, jumpSpeed);
         }
     }
 }
