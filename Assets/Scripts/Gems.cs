f935b9ef806dff808ad1108a4b7ab11b25eb324b
YashKeni <yashkeni007@gmail.com>
2023-09-28 14:24:55 +0530

    Added a gem collection animation

--- Assets/Scripts/Gems.cs
+++ Assets/Scripts/Gems.cs
@@ -6,10 +6,17 @@ public class Gems : MonoBehaviour
 {
     [SerializeField] AudioClip gemPickupSFX;
     [SerializeField] int gemValue = 100;
+    [SerializeField] float animDelay = 0.1f;
 
     bool hasCollected = false;
 
     AudioSource gemSound;
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -20,9 +27,12 @@ public class Gems : MonoBehaviour
             FindObjectOfType<GameSession>().AddToScore(gemValue);
             gemSound = GameObject.Find("SFX AudioSource").GetComponent<AudioSource>();
             gemSound.PlayOneShot(gemPickupSFX, 1f);
-
-            gameObject.SetActive(false);
-            Destroy(gameObject);
+            animator.SetTrigger("hasPickedUp");
         }
+    }
+
+    public void DestroyAfterPickup()
+    {
+        Destroy(gameObject);
     }
 }
