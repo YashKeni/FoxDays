7568bf6a911b93dbc15a142886ea2b6af1f69534
YashKeni <yashkeni007@gmail.com>
2023-10-08 17:16:55 +0530

    Created a script for main menu buttons

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void OpenLevelSelection()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void OpenSettings()
+    {
+        Debug.Log("Settings Opened");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
