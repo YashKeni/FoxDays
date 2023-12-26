3b1d94ba250dd7801ef9372190f6514727ebc4f5
YashKeni <yashkeni007@gmail.com>
2023-12-26 19:56:46 +0530

    Updated version

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -18,6 +18,10 @@ public class MainMenu : MonoBehaviour
 
     public void QuitGame()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
